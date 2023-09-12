3c65bc57e3822c965189bed5d7eef9dbe59f3701
Jorge Bejar <jorge@wyeworks.com>
2023-09-12 16:35:39 -0300

    Only-DSL custom widget

---
+++ src/carrousel.rs
@@ -0,0 +1,18 @@
+use makepad_widgets::*;
+
+live_design! {
+    import makepad_widgets::base::*;
+    import makepad_widgets::theme_desktop_dark::*;
+
+    Carrousel = <View> {
+        width: 200,
+        height: 100,
+        draw_bg: {color: #550}
+
+        align: {x: 0.5, y: 0.5},
+
+        <Label> {
+            text: "Hello World"
+        }
+    }
+}
