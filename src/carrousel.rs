e1fc334d43a913730a42d272936f34343c4e6133
Jorge Bejar <jorge@wyeworks.com>
2023-09-12 23:26:37 -0300

    Restore view definition

--- src/carrousel.rs
+++ src/carrousel.rs
@@ -5,17 +5,19 @@ live_design! {
     import makepad_widgets::theme_desktop_dark::*;
 
     Carrousel = {{Carrousel}} {
-        image1: <Image> {
+        flow: Down,
+
+        image1 = <Image> {
             width: 400
             height: 266
             source: dep("crate://self/resources/image1.png")
         }
-        image2: <Image> {
+        image2 = <Image> {
             width: 400
             height: 266
             source: dep("crate://self/resources/image2.png")
         }
-        image3: <Image> {
+        image3 = <Image> {
             width: 400
             height: 266
             source: dep("crate://self/resources/image3.png")
@@ -25,14 +27,8 @@ live_design! {
 
 #[derive(Live)]
 pub struct Carrousel {
-    #[live]
-    image1: Image,
-
-    #[live]
-    image2: Image,
-
-    #[live]
-    image3: Image,
+    #[deref]
+    view: View,
 }
 
 impl LiveHook for Carrousel {
@@ -42,23 +38,11 @@ impl LiveHook for Carrousel {
 }
 
 impl Widget for Carrousel {
-    fn redraw(&mut self, _cx: &mut Cx) {
+    fn redraw(&mut self, cx: &mut Cx) {
+        self.view.redraw(cx);
     }
 
     fn draw_walk_widget(&mut self, cx: &mut Cx2d, walk: Walk) -> WidgetDraw {
-        cx.begin_turtle(walk, Layout {flow: Flow::Down, ..Layout::default()});
-
-        let walk1 = self.image1.walk(cx);
-        self.image1.draw_walk_widget(cx, walk1) ?;
-
-        let walk2 = self.image2.walk(cx);
-        self.image2.draw_walk_widget(cx, walk2) ?;
-
-        let walk3 = self.image3.walk(cx);
-        self.image3.draw_walk_widget(cx, walk3) ?;
-
-        cx.end_turtle();
-
-        WidgetDraw::done()
+        self.view.draw_walk_widget(cx, walk)
     }
 }
