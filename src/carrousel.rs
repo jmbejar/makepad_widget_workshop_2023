98e3723f72ebcfbced74fb895da27d912f5b1db6
Jorge Bejar <jorge@wyeworks.com>
2023-09-13 13:04:42 -0300

    Make carrousel widget easier to customize

--- src/carrousel.rs
+++ src/carrousel.rs
@@ -7,7 +7,7 @@ live_design! {
     ImageContainer = <View> {
         width: Fit,
         height: Fit,
-        visible: false,
+        // visible: false,
         image = <Image> {
             width: 400
             height: 266
@@ -15,29 +15,37 @@ live_design! {
     }
 
     Carrousel = {{Carrousel}} {
-        flow: Down,
+        //flow: Down,
 
-        pages: [
-            page1,
-            page2,
-            page3,
+        images: [
+            dep("crate://self/resources/image1.png"),
+            dep("crate://self/resources/image2.png"),
+            dep("crate://self/resources/image3.png")
         ]
 
-        page1 = <ImageContainer> {
-            image = {
-                source: dep("crate://self/resources/image1.png")
-            }
-        }
-        page2 = <ImageContainer> {
-            image = {
-                source: dep("crate://self/resources/image2.png")
-            }
-        }
-        page3 = <ImageContainer> {
-            image = {
-                source: dep("crate://self/resources/image3.png")
-            }
-        }
+        page_template: <ImageContainer> {}
+
+        // pages: [
+        //     page1,
+        //     page2,
+        //     page3,
+        // ]
+
+        // page1 = <ImageContainer> {
+        //     image = {
+        //         source: dep("crate://self/resources/image1.png")
+        //     }
+        // }
+        // page2 = <ImageContainer> {
+        //     image = {
+        //         source: dep("crate://self/resources/image2.png")
+        //     }
+        // }
+        // page3 = <ImageContainer> {
+        //     image = {
+        //         source: dep("crate://self/resources/image3.png")
+        //     }
+        // }
     }
 }
 
@@ -50,17 +58,29 @@ pub enum CarrouselPageOrder {
 
 #[derive(Live)]
 pub struct Carrousel {
-    #[deref]
-    view: View,
+    #[rust] area: Area,
+    // #[deref]
+    // view: View,
 
     #[live]
     page_order: CarrouselPageOrder,
 
+    #[live]
+    images: Vec<LiveDependency>,
+
+    #[live]
+    page_template: Option<LivePtr>,
+
     #[rust(0)]
     current_page: u8,
 
-    #[live]
-    pages: Vec<LiveId>
+    // #[live]
+    // pages: Vec<LiveId>
+    #[rust]
+    pages: ComponentMap<LiveId,WidgetRef>
+
+    // #[live]
+    // pages: Vec<LiveId>
 }
 
 impl LiveHook for Carrousel {
@@ -68,10 +88,25 @@ impl LiveHook for Carrousel {
         register_widget!(cx, Carrousel);
     }
 
-    fn after_apply(&mut self, _cx: &mut Cx, from: ApplyFrom, _index: usize, _nodes: &[LiveNode]) {
+    // fn after_apply(&mut self, _cx: &mut Cx, from: ApplyFrom, _index: usize, _nodes: &[LiveNode]) {
+    //     if from.is_from_doc()  {
+    //         let current_page_live_id = self.pages[self.current_page as usize];
+    //         self.view.view(&[current_page_live_id]).set_visible(true);
+    //     }
+    // }
+
+    fn after_apply(&mut self, cx: &mut Cx, from: ApplyFrom, _index: usize, _nodes: &[LiveNode]) {
         if from.is_from_doc()  {
-            let current_page_live_id = self.pages[self.current_page as usize];
-            self.view.view(&[current_page_live_id]).set_visible(true);
+            for (idx, image_dep) in self.images.iter().enumerate() {
+                let widget_id = LiveId::from_str(&format!("page{}", idx));
+                let page = self.pages.get_or_insert(cx, widget_id, |cx| {
+                    WidgetRef::new_from_ptr(cx, self.page_template)
+                });
+
+                page
+                    .image(id!(image))
+                    .load_image_dep_by_path(cx, &image_dep.as_str());
+            }
         }
     }
 }
@@ -83,13 +118,13 @@ impl Widget for Carrousel {
         event: &Event,
         _dispatch_action: &mut dyn FnMut(&mut Cx, WidgetActionItem),
     ) {
-        match event.hits(cx, self.view.area()) {
+        match event.hits(cx, self.area) {
             Hit::FingerUp(fe) => if fe.is_over {
                 self.update_current_page();
-                self.reset_frames_visibility();
+                //self.reset_frames_visibility();
 
-                let current_page_live_id = self.pages[self.current_page as usize];
-                self.view.view(&[current_page_live_id]).set_visible(true);
+                // let current_page_live_id = self.pages[self.current_page as usize];
+                // self.view.view(&[current_page_live_id]).set_visible(true);
 
                 self.redraw(cx);
             }
@@ -98,28 +133,38 @@ impl Widget for Carrousel {
     }
 
     fn redraw(&mut self, cx: &mut Cx) {
-        self.view.redraw(cx);
+        self.area.redraw(cx);
     }
 
     fn draw_walk_widget(&mut self, cx: &mut Cx2d, walk: Walk) -> WidgetDraw {
-        self.view.draw_walk_widget(cx, walk)
+        //self.view.draw_walk_widget(cx, walk)
+        self.draw_walk(cx, walk);
+        WidgetDraw::done()
     }
 }
 
 impl Carrousel {
-    fn reset_frames_visibility(&mut self) {
-        for page_id in &self.pages {
-            self.view.view(&[*page_id]).set_visible(false);
-        }
+    fn draw_walk(&mut self, cx: &mut Cx2d, walk: Walk) {
+        cx.begin_turtle(walk, Layout::default());
+
+        let widget_id = LiveId::from_str(&format!("page{}", self.current_page));
+        let page = self.pages.get_or_insert(cx, widget_id, |cx| {
+            WidgetRef::new_from_ptr(cx, self.page_template)
+        });
+
+        let _ = page.draw_walk_widget(cx, walk);
+        cx.end_turtle_with_area(&mut self.area);
     }
 
     fn update_current_page(&mut self) {
         match self.page_order {
             CarrouselPageOrder::Normal => {
-                self.current_page = (self.current_page + 1) % self.pages.len() as u8;
+                //self.current_page = (self.current_page + 1) % self.pages.len() as u8;
+                self.current_page = (self.current_page + 1) % self.images.len() as u8;
             }
             CarrouselPageOrder::Reverse => {
-                self.current_page = (self.current_page + self.pages.len() as u8 - 1) % self.pages.len() as u8;
+                //self.current_page = (self.current_page + self.pages.len() as u8 - 1) % self.pages.len() as u8;
+                self.current_page = (self.current_page + self.images.len() as u8 - 1) % self.images.len() as u8;
             }
         }
     }
