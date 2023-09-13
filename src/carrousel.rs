98e1cbb02bb21399b0b4b9796b4a7cf8dd0e2d2a
Jorge Bejar <jorge@wyeworks.com>
2023-09-12 22:29:19 -0300

    Example using explicit rendering of images

--- src/carrousel.rs
+++ src/carrousel.rs
@@ -5,18 +5,17 @@ live_design! {
     import makepad_widgets::theme_desktop_dark::*;
 
     Carrousel = {{Carrousel}} {
-        flow: Down
-        <Image> {
+        image1: <Image> {
             width: 400
             height: 266
             source: dep("crate://self/resources/image1.png")
         }
-        <Image> {
+        image2: <Image> {
             width: 400
             height: 266
             source: dep("crate://self/resources/image2.png")
         }
-        <Image> {
+        image3: <Image> {
             width: 400
             height: 266
             source: dep("crate://self/resources/image3.png")
@@ -26,8 +25,14 @@ live_design! {
 
 #[derive(Live)]
 pub struct Carrousel {
-    #[deref]
-    view: View,
+    #[live]
+    image1: Image,
+
+    #[live]
+    image2: Image,
+
+    #[live]
+    image3: Image,
 }
 
 impl LiveHook for Carrousel {
@@ -37,11 +42,23 @@ impl LiveHook for Carrousel {
 }
 
 impl Widget for Carrousel {
-    fn redraw(&mut self, cx: &mut Cx) {
-        self.view.redraw(cx);
+    fn redraw(&mut self, _cx: &mut Cx) {
     }
 
     fn draw_walk_widget(&mut self, cx: &mut Cx2d, walk: Walk) -> WidgetDraw {
-        self.view.draw_walk_widget(cx, walk)
+        cx.begin_turtle(walk, Layout {flow: Flow::Down, ..Layout::default()});
+
+        let walk1 = self.image1.walk(cx);
+        self.image1.draw_walk_widget(cx, walk1) ?;
+
+        let walk2 = self.image2.walk(cx);
+        self.image2.draw_walk_widget(cx, walk2) ?;
+
+        let walk3 = self.image3.walk(cx);
+        self.image3.draw_walk_widget(cx, walk3) ?;
+
+        cx.end_turtle();
+
+        WidgetDraw::done()
     }
 }
